277be71486e5cc40054152b59213202accf9c604
Ultimate553 <diego.torred@gmail.com>
2024-02-27 20:27:31 -0600

    More tests

--- src/main.js
+++ src/main.js
@@ -1,19 +1,45 @@
-const {Blockchain, Transaction}  = require('./blockchain');
+const { Blockchain, Transaction } = require('./blockchain');
 const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
 
-const myKey = ec.keyFromPrivate('7c2202f947185d1fd1e77a493660d17f3761b1180fb5dde02e46ec096655a143');
+// Your private key goes here
+const myKey = ec.keyFromPrivate(
+  '7c4c45907dec40c91bab3480c39032e90049f1a44f3e18c3e07c23e3273995cf'
+);
+
+// From that we can calculate your public key (which doubles as your wallet address)
 const myWalletAddress = myKey.getPublic('hex');
 
-let ultiCoin  = new Blockchain();
+// Create new instance of Blockchain class
+const dammCoin = new Blockchain();
 
-const tr1 = new Transaction(myWalletAddress, 'To address public key here', 10);
-tr1.signTransaction(myKey);
-ultiCoin.addTransaction(tr1);
-console.log("\n Miner is cooking...");
-ultiCoin.minePendingTransactions(myWalletAddress);
+// Mine first block
+dammCoin.minePendingTransactions(myWalletAddress);
 
-console.log("Alex now has a balance of " + ultiCoin.getBalanceOfAddress(myWalletAddress));
+// Create a transaction & signTransaction it with your key
+const tx1 = new Transaction(myWalletAddress, 'address2', 100);
+tx1.signTransaction(myKey);
+dammCoin.addTransaction(tx1);
 
-console.log('Is blockchain valid? ' + ultiCoin.isChainValid());
+// Mine block
+dammCoin.minePendingTransactions(myWalletAddress);
+
+// Create second transaction
+const tx2 = new Transaction(myWalletAddress, 'address1', 50);
+tx2.signTransaction(myKey);
+dammCoin.addTransaction(tx2);
+
+// Mine block
+dammCoin.minePendingTransactions(myWalletAddress);
+
+console.log();
+console.log(
+  `Balance of Diego is ${dammCoin.getBalanceOfAddress(myWalletAddress)}`
+);
+
+dammCoin.chain[1].transactions[0].amount = 10;
+
+// Check if the chain is valid
+console.log();
+console.log('Blockchain valid?', dammCoin.isChainValid() ? 'Yes' : 'No');
 
